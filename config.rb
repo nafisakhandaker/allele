66a646a73183b0032cbdade2660d23fbf923cdb5
Nafisa Khandaker <nkhandaker@partners.org>
2017-07-19 15:11:29 -0400

    t

--- config.rb
+++ config.rb
@@ -243,11 +243,11 @@ end
 
 end
 
-  set :css_dir, 'https://nafisakhandaker.github.io/clingen-data-model.github.io/static-resource/stylesheets/'
+  set :css_dir, '/clingen-data-model.github.io/static-resource/stylesheets/'
   
-  set :js_dir, 'https://nafisakhandaker.github.io/clingen-data-model.github.io/static-resource/javascripts/'
+  set :js_dir, '/clingen-data-model.github.io/static-resource/javascripts/'
   
-  set :images_dir, 'https://nafisakhandaker.github.io/clingen-data-model.github.io/static-resource/images/'
+  set :images_dir, '/clingen-data-model.github.io/static-resource/images/'
 
 # Build-specific configuration
 configure :build do
